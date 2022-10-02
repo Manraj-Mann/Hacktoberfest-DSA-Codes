efc4b6fd6f169bc12cfc562c528ef2bfeb802d2a
ManrajMann <mannmanraj239@gmail.com>
2022-10-02 18:40:32 +0530

    added more questions in trees

---
+++ C++/Trees/questions/ques3.cpp
@@ -0,0 +1,119 @@
+// Give an algorithm for searching an element in binary tree.
+
+#include <climits>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+};
+
+node *init(int data)
+{
+    node *n = new node;
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+void insert_left(node *root, int data)
+{
+    root->left = init(data);
+}
+void insert_right(node *root, int data)
+{
+    root->right = init(data);
+}
+
+void delete_left(node *root)
+{
+
+    node *todel = root->left;
+    root->left = NULL;
+    delete todel;
+}
+void delete_right(node *root)
+{
+
+    node *todel = root->right;
+    root->right = NULL;
+    delete todel;
+}
+
+
+bool find(node * root , int data)
+{
+    bool temp;
+
+    if (root == NULL )
+    {
+        return 0;
+    }
+    else{
+
+
+        if (root->data == data)
+        {
+            return 1;
+        }
+        else{
+
+            temp = find(root->left , data);
+            if (temp != 0)
+            {
+                return temp;
+            }
+            else{
+
+                return find(root->right , data);
+            }
+            
+        }
+        
+    }
+
+    return 0;
+    
+}
+int main()
+{
+    node *root = NULL;
+
+    root = init(1); // root node
+
+    //       1
+
+    insert_left(root, 2);
+    insert_right(root, 3);
+
+    //        1
+    //       / \'
+    //      2   3
+
+    insert_left(root->left, 4);
+    insert_right(root->left, 5);
+
+    //        1
+    //       / \'
+    //      2   3
+    //      / \'
+    //     4   5
+
+    insert_left(root->right, 6);
+    insert_right(root->right, 7);
+
+    //        1
+    //       / \'
+    //       2   3
+    //      / \' / \'
+    //     4   5 6  7
+
+    cout << "Found - " << find(root , 6);
+
+    return 0;
+}
