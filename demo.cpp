618df543f47e167861e116a9c5e5724f3a25e415
ManrajMann <mannmanraj239@gmail.com>
2022-10-04 15:58:32 +0530

    Added demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,25 @@
+/*Author : Manraj Singh */
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Problem : 	
+/* Solution : 	 */
+class Solution { 
+
+    public: 
+
+
+};
+
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+
+    return 0;
+}
